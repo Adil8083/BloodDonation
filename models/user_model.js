184f6adfe4bec1ffdee7ec25dd6f83b4457f3da5
Adil8083 <adilwahed@outlook.com>
2019-11-27 00:05:17 +0500

    database

---
+++ models/user_model.js
@@ -0,0 +1,16 @@
+const mongoos=require('mongoose');
+const Schema = mongoos.Schema;
+
+const User=new Schema({
+    id:Schema.Types.ObjectId,
+    UserName:{type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+    BloodType:{type:String,required:true},
+    Address:{type:String,required:true},
+    Phone_Number:{type:Number,required:true},
+    CNIC:{type:Number,required:true},
+    is_Reuqested:Boolean,
+    NoOFDonations:Number
+});
+
+module.exports=mongoos.model("User_Infos",User)
