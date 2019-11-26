2fc4b945e4293579d0488b69e12bee067519831a
Adil8083 <adilwahed@outlook.com>
2019-11-27 00:34:42 +0500

    add password field

--- models/user_model.js
+++ models/user_model.js
@@ -1,16 +1,17 @@
-const mongoos=require('mongoose');
+const mongoos = require('mongoose');
 const Schema = mongoos.Schema;
 
-const User=new Schema({
-    id:Schema.Types.ObjectId,
-    UserName:{type:String,required:true,unique:true},
-    email:{type:String,required:true,unique:true},
-    BloodType:{type:String,required:true},
-    Address:{type:String,required:true},
-    Phone_Number:{type:Number,required:true},
-    CNIC:{type:Number,required:true},
-    is_Reuqested:Boolean,
-    NoOFDonations:Number
+const User = new Schema({
+    id: Schema.Types.ObjectId,
+    UserName: { type: String, required: true, unique: true },
+    Password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    BloodType: { type: String, required: true },
+    Address: { type: String, required: true },
+    Phone_Number: { type: Number, required: true },
+    CNIC: { type: Number, required: true },
+    is_Reuqested: Boolean,
+    NoOFDonations: Number
 });
 
-module.exports=mongoos.model("User_Infos",User)
+module.exports = mongoos.model("User_Infos", User)
