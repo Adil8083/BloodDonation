9f527566ef3fcc71501a6895444b43b56d8ec374
Adil8083 <adilwahed@outlook.com>
2019-11-28 22:49:21 +0500

    fix mongod

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,90 +1,61 @@
-const express=require('express');
-const route=express();
+const express = require('express');
+const route = express();
 route.use(express.json());
 const user = require('../models/user_model');
-const bcrypt=require('bcrypt');
-route.post("/createUser", 
-(req, res, next) => {
-    user.find({UserName:req.body.UserName}, (err,data) =>{
+const bcrypt = require('bcrypt');
+route.post("/createUser", async (req, res, next) => {
+    user.find({ UserName: req.body.UserName }, (err, data) => {
         if (Object.getOwnPropertyNames(data).length === 1) {
-            return next();     
-        }
-        else {
+            return next();
+        } else {
             res.send({
                 success: false,
-                message: "This Name is already taken,Please Enter other Email"
+                message: "This Name is already taken,Please Enter other Name"
             });
         }
     })
-        
-},
-
-(req,res,next) => {
-    user.find({email:req.body.email}, (err,data) =>{
+}, async (req, res, next) => {
+    user.find({ email: req.body.email }, (err, data) => {
         if (Object.getOwnPropertyNames(data).length === 1) {
-            return next();     
-        }
-        else {
+            return next();
+        } else {
             res.send({
                 success: false,
                 message: "This email is already taken,Please Enter other Email"
             });
         }
     })
-
-},
-(req,res,next) => {
-    user.find({Phone_Number:req.body.Phone_Number}, (err,data) =>{
+}, async (req, res, next) => {
+    user.find({ Phone_Number: req.body.Phone_Number }, (err, data) => {
         if (Object.getOwnPropertyNames(data).length === 1) {
-            return next();     
-        }
-        else {
+            return next();
+        } else {
             res.send({
                 success: false,
                 message: "This Phone Number is already taken,Please Enter other Phone Number"
             });
         }
     })
-
-},
-(req,res,next) =>{
-    if (req.body.UserName == req.body.Password){
+}, async (req, res, next) => {
+    if (req.body.UserName == req.body.Password) {
         res.send({
-            success:false,
-            message: "Please password which is different from Name"
+            success: false,
+            message: "Please enter password which is different from Name"
         });
-    }
-    else {
+    } else {
         return next()
     }
-},
-(req,res,next) => {
-    bcrypt.hash(req.body.Password,10,(err,hash)=>{
-        if (err){
-                res.send({
-                    success:false,
-                    message:"Please Enter password again.."
-            });
-            }
-            else {
-                console.log();
-                req.body.Password =hash;
-                res.send({
-                    success: true,
-                    message: req.body
-                })
-                const o = new user(req.body );
-                console.log(o.save());
-            }
-        });
+}, async (req, res, next) => {
+    console.log();
+    const users = new user(req.body);
+    const salt = await bcrypt.genSalt(10);
+    users.Password = await bcrypt.hash(users.Password, salt);
+    await users.save();
+    res.send({
+        success: true,
+        message: req.body
+    })
 }
-
 );
-
-
-
-
-
-
 
 module.exports = route;
